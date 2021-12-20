f6de563ba371617cddeccc78036f6172bccf7ec2
Madz No <madzno12@gmail.com>
2021-12-19 20:24:54 -0500

    adding easy 1 exercises

---
+++ easy1_10.rb
@@ -0,0 +1,11 @@
+def calculate_bonus(salary, boolean)
+  if boolean == true
+    bonus = salary / 2
+  else
+    bonus = 0
+  end
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
--- easy1_7.rb
+++ easy1_7.rb
@@ -1,30 +1,15 @@
-#input = a positive integer
-# output = a string of alternating 1's and 0's which length
-# is = to string
-# rules
-# the length of the string should = the integer given as an argument
-# the string always starts with 1
-# data structure - order matters so its possible an array will help us
-# algo
-# 1. set integer = methods parameter
-# 2. create a variable called string = to an empty string
-# 3. set a "counter" set to 0
-# 4. until strings length is == integer add "1" to string on even counters
-# and add 1 on odd counters
-# return the string
-
 def stringy(integer)
-  string = ''
-  starting_index = 0
-until string.length == integer
-  if starting_index % 2 == 0
-    string << "1"
-  else
-    string << "0"
+  array_of_numbers = []
+  index = 0
+  until index == integer
+    if index.even?
+      array_of_numbers << "1"
+    else
+      array_of_numbers << "0"
+    end
+    index += 1
   end
-  starting_index += 1
-end
- string
+  results = array_of_numbers.join
 end
 
 puts stringy(6) == '101010'
@@ -32,13 +17,27 @@ puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
 
-def stringy(size)
+def stringy2(size, default = 1)
   numbers = []
 
-  size.times do |index|
-    number = index.even? ? 1 : 0
-    numbers << number
+  if default == 1
+    size.times do |index|
+      number = index.even? ? 1 : 0
+      numbers << number
+    end
+  end
+
+  if default == 0
+    size.times do |index|
+      number = index.even? ? 0 : 1
+      numbers << number
+    end
   end
 
   numbers.join
 end
+
+puts stringy2(6, 0)
+puts stringy2(9)
+puts stringy2(4, 1)
+puts stringy2(7, 0)
