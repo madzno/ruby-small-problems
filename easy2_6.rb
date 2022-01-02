cf59527c055aecdeedae88ac00a115902eaca655
Madz No <madzno12@gmail.com>
2022-01-02 13:05:24 -0500

    adding easy 2 problems

---
+++ easy2_6.rb
@@ -0,0 +1,19 @@
+#(1..99).each do |num|
+  #puts num if num % 2 != 0
+#end
+
+#further exploration
+#value = 1
+#while value <= 99
+  #puts value if value.odd?
+  #value += 1
+#end
+
+#1.upto(99) do |num|
+#  puts num if num.odd?
+#end
+
+#a = []
+#1.upto(99) { |num| a << num }
+#a.select! { |num| num.odd? }
+#puts a
