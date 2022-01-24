35009fc88d3a910067c2acd4219c7171f3b47789
Madz No <madzno12@gmail.com>
2022-01-24 17:12:51 -0500

    udating

--- easy1_3.rb
+++ easy1_3.rb
@@ -1,8 +1,19 @@
+# def digit_list(number)
+#   number.digits.reverse
+# end
+
+# def digit_list(number)
+#   number.to_s.chars.map(&:to_i)
+# end
+
 def digit_list(number)
-  num_list = number.to_s.chars
-  num_list.map do |num|
-    num.to_i
+  digits = []
+  loop do
+    number, remainder = number.divmod(10)
+    digits.prepend(remainder)
+    break if number == 0
   end
+  digits
 end
 
 puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
@@ -10,6 +21,4 @@ puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
 
-def digit_list2(number)
-  number.digits.reverse
-end
+
