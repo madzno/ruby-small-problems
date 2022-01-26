a27769af944ed06e153fe4994c07d5a8a035510a
Madz No <madzno12@gmail.com>
2022-01-26 08:05:55 -0500

    adding easy 5

---
+++ easy5_2.rb
@@ -0,0 +1,21 @@
+MINUTES_PER_HOUR = 60
+HOURS_PER_DAY = 24
+MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
+
+def normalize_minute(minutes)
+  while minutes < 0
+    minutes += MINUTES_PER_DAY
+  end
+
+  minutes % MINUTES_PER_DAY
+end
+
+p normalize_minute(-4321)
+
+# time_of_day(0) == "00:00"
+# time_of_day(-3) == "23:57"
+# time_of_day(35) == "00:35"
+# time_of_day(-1437) == "00:03"
+# time_of_day(3000) == "02:00"
+# time_of_day(800) == "13:20"
+# time_of_day(-4231) == "01:29"
