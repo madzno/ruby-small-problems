86f8d7123c791bb78511b14d262df48593b795c3
Madz No <madzno12@gmail.com>
2022-02-11 13:07:37 -0500

    updating easy 5

--- easy_5/easy5_9.rb
+++ easy_5/easy5_9.rb
@@ -33,25 +33,70 @@ of that integer
 - Define a method 'alphabetic_number_sort' with one parameter that represents
 the input array
 
-- initialize a variable `sorted_words` equal to an array of the values in the
-integers_to_strings constant hash, sort the words array of strings
-
-- initialize a variable `input_words` to an empty array
-
-- initialize a variable `result_hash` to an empty array
+- initialize a variable `result_array``
 
 - For each number in the input array, determine what english word
 it is associated with
-   - look up the key that the is the same as the input integer and add
-   that key's value to the input words array
+   - look up the key in integers_to_strings that the is the same as the input integer and
+   add that element to the results_array
 
-- for each element in the input words array
+- sort! the result_array array, now made up of strings from the integers_to_strings hash
 
-- look up that word in the constant 'words'
+- iterate through the integers to strings hash and set each key equal to the current value
+and value equal to the key set this to the strings to integers hash
+
+- transform each element in the result_array back to the integer key that it represents
+  - look up the key in the strings_to_integers hash that is the same as the input string
+  and transform (destructively) that element into its string value
+
+- return the results_array
+
+hash
 =end
 
+INTEGERS_TO_STRINGS = {
+                       0 => 'zero',
+                       1 => 'one',
+                       2 => 'two',
+                       3 => 'three',
+                       4 => 'four',
+                       5 => 'five',
+                       6 => 'six',
+                       7 => 'seven',
+                       8 => 'eight',
+                       9 => 'nine',
+                       10 => 'ten',
+                       11 => 'eleven',
+                       12 => 'twelve',
+                       13 => 'thirteen',
+                       14 => 'fourteen',
+                       15 => 'fifteen',
+                       16 => 'sixteen',
+                       17 => 'seventeen',
+                       18 => 'eighteen',
+                       19 => 'nineteen'
+                       }
 
-alphabetic_number_sort((0..19).to_a) == [
+def alphabetic_number_sort(arr)
+  result_array = []
+
+  arr.each do |num|
+    result_array << INTEGERS_TO_STRINGS[num]
+  end
+  result_array.sort!
+
+  strings_to_integers = {}
+  INTEGERS_TO_STRINGS.each do |k, v|
+    strings_to_integers[v] = k
+  end
+
+
+  result_array.map! do |string|
+    string = strings_to_integers[string]
+  end
+end
+
+p alphabetic_number_sort((0..19).to_a) == [
   8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17,
   6, 16, 10, 13, 3, 12, 2, 0
 ]
