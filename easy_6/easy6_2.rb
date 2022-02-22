65ce0447c95898acaeedbb7a4bef3862df46ae55
Madz No <madzno12@gmail.com>
2022-02-22 18:36:40 -0500

    updating easy

--- easy_6/easy6_2.rb
+++ easy_6/easy6_2.rb
@@ -35,3 +35,7 @@ end
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+
+
+
