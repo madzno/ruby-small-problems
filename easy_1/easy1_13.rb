78cf9914cc7d9502f20455b70ee6d1f67c1c7c03
Madz No <madzno12@gmail.com>
2022-03-05 17:36:26 -0500

    updating easy 1

---
+++ easy_1/easy1_13.rb
@@ -0,0 +1,12 @@
+def digit_list(num)
+  array = num.to_s.chars
+  array.map do |string|
+    string.to_i
+  end
+end
+
+
+puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
+puts digit_list(7) == [7]                     # => true
+puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts digit_list(444) == [4, 4, 4]             # => true
