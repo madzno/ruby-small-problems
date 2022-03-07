a69f63077998723181ea13859308729cd2b5234e
Madz No <madzno12@gmail.com>
2022-03-07 00:36:55 -0500

    updating

--- easy_9/easy9_11.rb
+++ easy_9/easy9_11.rb
@@ -19,18 +19,43 @@ all words four letters
 Data Structure
 initialize an empty hash variable called anagrams
 
-iterate through the words array and for each word split the word
-into characters, sort the characters and join
 
 if the hash has the current key,
 
 Algo
 
+initialize an empty hash variable result_hash
+
+iterate over the input array and assign
+
+a variable 'key' to the return value of splitting each word into characters and sorting
+those characters and then joining the results
+
+if the result_hash has a key that matches the current key, then add the current
+word to that key's value
+
+if the result_hash does not have a key that matches the current key, then add the current
+key as a key and set its value equal to an array containing that key
+
+iterate over each value in the result hash and output it
+
 =end
 
 def anagrams(arr)
+  result_hash = {}
+  arr.each do |word|
+    key = word.chars.sort.join
+    if result_hash.has_key?(key)
+      result_hash[key] << word
+    else
+      result_hash[key] = [word]
+    end
+  end
+  result_hash.each { |k, v| p v }
 end
+
 words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
 
+anagrams(words)
